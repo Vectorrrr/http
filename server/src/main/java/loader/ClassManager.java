fee27a55234f810cbff650aaedee3aea08687531
Vanya <gladush97@gmail.com>
2016-04-13 17:19:40 +0300

    Add commentary and optimized code

--- server/src/main/java/loader/ClassManager.java
+++ server/src/main/java/loader/ClassManager.java
@@ -2,8 +2,12 @@ package loader;
 
 import holder.ProcessorHolder;
 import org.apache.log4j.Logger;
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
 import page.PageProcessor;
 
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 import java.io.*;
 
 /**
@@ -17,18 +21,27 @@ import java.io.*;
  */
 public class ClassManager {
     private static final Logger log = Logger.getLogger(ClassManager.class);
-    private static final String CLASS = ".class";
     private static final PropertyLoader PROPERTY_LOADER = PropertyLoader.getPropertyLoader("server.configuration.properties");
-    private static final String DIRECTORY_FOR_STORE_FILE = PROPERTY_LOADER.property("directory.for.store.class");
-    private static final String MANIFEST_FILE_NAME = PROPERTY_LOADER.property("class.manager.file");
-    private static final String ONE_LINE = "%s %s\n";
-    private static final String PATH_IN_DIRECTORY = DIRECTORY_FOR_STORE_FILE + "/%s";
+
+    private static final String EXCEPTION_IN_PARSE_FILE = "Exception in pars file %s";
     private static final String EXCEPTION_ADD_CLASS = "When you add class you have error %s";
+
+    private static final String DIRECTORY_FOR_STORE_FILE = PROPERTY_LOADER.property("directory.for.store.class");
+    private static final String PATH_IN_DIRECTORY = DIRECTORY_FOR_STORE_FILE + "/%s";
+    private static final String DEFAULT_UNPACK_DIR = PROPERTY_LOADER.property("default.unpack.dir");
+    private static final String MANIFEST_FILE_NAME = PROPERTY_LOADER.property("class.manager.file");
+
     private static final String CANT_CREATE_PROCESS_HOLDER = "Can init processor holder because %s";
-    private static final String SPACE = " ";
     private static final String CANT_CREATE_FILE = "I can't create new file %s";
+
+    private static final String SPACE = " ";
+    private static final String ONE_LINE = "%s %s\n";
+    private static final String SITE_URL = "/%s%s";
+    private static final String CLASS = ".class";
+
     private static Integer countClass = 0;
     private static boolean manifestExist = false;
+    private static String siteName;
 
     /**
      * Create a directory to save classes
@@ -37,6 +50,13 @@ public class ClassManager {
         new File(DIRECTORY_FOR_STORE_FILE).mkdirs();
     }
 
+    /**
+     * The method takes the name of the URL class,
+     * which it processes and the content of the class.
+     * Information is recorded in this class server
+     * manifest, as well as the contents of the class
+     * is stored in a specific location on the server
+     */
     public static void addClass(String url, String className, byte[] classContent) {
         try {
             writeInManifest(url, className);
@@ -46,6 +66,10 @@ public class ClassManager {
         }
     }
 
+    /**
+     * The method of reading the manifesto and on the basis
+     * of its content adds new processes in ProcessorHolder
+     */
     public static void initProcessorHolder() {
         try {
             if (createManifest()) {
@@ -56,8 +80,11 @@ public class ClassManager {
         }
     }
 
+    /**
+     * Method writes class server class storage while increasing countClass 1
+     */
     private static void writeClass(byte[] classContent) throws IOException {
-        File f = new File(String.format(PATH_IN_DIRECTORY, getCountClass()));
+        File f = new File(getPathInDirectory(getCountClass()));
         createFile(f);
         try (FileOutputStream fw = new FileOutputStream(f)) {
             fw.write(classContent);
@@ -102,7 +129,10 @@ public class ClassManager {
     }
 
 
-
+    /**
+     * The method parses the server and on the basis of
+     * a manifesto and it adds objects ProcessorHolder
+     */
     private static void parseManifest() throws IOException {
         try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(getManifestFile())))) {
             String line;
@@ -118,8 +148,54 @@ public class ClassManager {
         countClass++;
         return o;
     }
-    private static String getPathInDirectory(String name){
+
+    private static String getPathInDirectory(String name) {
         return String.format(PATH_IN_DIRECTORY, name);
+    }
+
+    /**
+     * Method shows whether the class
+     * is successfully loaded or not
+     */
+    public static boolean addLoadNewFile(File f, String siteNam) {
+        siteName = siteNam;
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser parser = factory.newSAXParser();
+            SaxHandler handler = new SaxHandler();
+            parser.parse(f, handler);
+        } catch (Exception e) {
+            log.error(String.format(EXCEPTION_IN_PARSE_FILE, e.getMessage()));
+            return false;
+        }
+        return true;
+    }
+
+    private static final class SaxHandler extends DefaultHandler {
+        public static final String EXCEPTION_CRETE_FILE = "I can't create instance class %s, because %s";
+        public FileClassLoader fileClassLoader = new FileClassLoader();
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attrs) {
+            if ("bean".equals(qName)) {
+                String classPath = attrs.getValue("classPath");
+                String url = String.format(SITE_URL, siteName, attrs.getValue("url"));
+                try {
+                    ProcessorHolder.addNewProcessor(url, getInstance(classPath));
+                    ClassManager.addClass(url, classPath, fileClassLoader.fetchClassFromFS(getDefaultClassPath(classPath)));
+                } catch (Exception e) {
+                    log.error(String.format(EXCEPTION_CRETE_FILE, classPath, e.getMessage()));
+                }
+            }
+        }
+
+        private PageProcessor getInstance(String classPath) {
+            return fileClassLoader.getInstance(getDefaultClassPath(classPath), classPath);
+        }
+
+        private String getDefaultClassPath(String classPath) {
+            return String.format("%s/%s.class", DEFAULT_UNPACK_DIR, classPath);
+        }
     }
 
 }
